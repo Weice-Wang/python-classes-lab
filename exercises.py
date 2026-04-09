c823a4bcfe0d38368f95df73847d08b127ba0aeb
weicewang <weicewang0712@gmail.com>
2026-04-10 09:57:10 +1000

    initializing

---
+++ exercises.py
@@ -0,0 +1,9 @@
+class game(): 
+    def __init__(self, turn, tie, winner, board):
+        self.turn = 'X'
+        self.tie = False
+        self.winner = None
+        self.board = {'a1': None, 'b1': None, 'c1': None,
+                      'a2': None, 'b2': None, 'c2': None,
+                      'a3': None, 'b3': None, 'c3': None,
+                      }
